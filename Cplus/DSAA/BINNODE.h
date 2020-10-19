5c8e64c69614f1bca07fe9723cc3fcc01276281e
Chris <41661134+jianhengLiu@users.noreply.github.com>
2020-10-19 09:44:51 +0800

    Add files via upload

---
+++ Cplus/DSAA/BINNODE.h
@@ -0,0 +1,16 @@
+#ifndef BINNODE_H
+#define BINNODE_H
+#endif // BINNODE_H
+template <typename E> class BinNode
+{
+public:
+    virtual ~BinNode() {}
+
+    virtual E& element() =0;
+    virtual void setElement(const E&) = 0;
+    virtual BinNode* left() const = 0;
+    virtual void setLeft(BinNode*) = 0;
+    virtual BinNode* right() const = 0;
+    virtual void setRight(BinNode*) = 0;
+    virtual bool isLeaf() = 0;
+}
---
+++ Cplus/DSAA/List.h
@@ -0,0 +1,7 @@
+#ifndef LIST_H
+#define LIST_H
+#endif // LIST_H
+template<typename E> class List
+{
+
+};
